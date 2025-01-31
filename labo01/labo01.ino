3d87caaa79ee66f29b68c366a00db4f13d795eea
cuzimapanda2003 <marcblais03@gmail.com>
2025-01-31 11:16:45 -0500

    debut du fade

--- labo01/labo01.ino
+++ labo01/labo01.ino
@@ -1,42 +1,61 @@
 
+
 #define LED_BUILDIN 13
+
 
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
   pinMode(LED_BUILDIN, OUTPUT);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(" Allume - 2168637");
+  
   light();
 
-  Serial.print("Clignotement – 2168637");
   blink();
 
-  Serial.print("Variation – 2168637")
+  fade();
 
 
 
 
 }
 
-void light(){
+void light()
+{
+Serial.println(" Allume - 2168637");
+
 digitalWrite(LED_BUILTIN, HIGH); 
 delay(2000);
 }
 
-void blink(){
-  // avant dernier chiffre = 3 
-
+void blink()
+{
+Serial.println("Clignotement – 2168637");
 for(int i = 0; i < 3; i++){
   digitalWrite(LED_BUILTIN, HIGH);  
   delay(250);                      
   digitalWrite(LED_BUILTIN, LOW);   
   delay(250);   
+}}
+
+void fade() {
+Serial.println("Éteint – 2142752 ");
+
+
+
 }
-}
+
+
+
+
+
+
+
