beacfce99cae2ac6b2373a7d19b3f29dd3c09c61
cuzimapanda2003 <marcblais03@gmail.com>
2025-01-31 10:46:43 -0500

    light et blink fait

--- labo01/labo01.ino
+++ labo01/labo01.ino
@@ -1,9 +1,42 @@
+
+#define LED_BUILDIN 13
+
+
+
 void setup() {
   // put your setup code here, to run once:
+
+  pinMode(LED_BUILDIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(" Allume - 2168637");
+  light();
 
+  Serial.print("Clignotement – 2168637");
+  blink();
+
+  Serial.print("Variation – 2168637")
+
+
+
+
+}
+
+void light(){
+digitalWrite(LED_BUILTIN, HIGH); 
+delay(2000);
+}
+
+void blink(){
+  // avant dernier chiffre = 3 
+
+for(int i = 0; i < 3; i++){
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(250);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(250);   
+}
 }
